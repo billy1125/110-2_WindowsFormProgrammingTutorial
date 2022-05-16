2f34be2b6fed480ded15a26f2885d7eae7d3a639
Cho-Hsun Lu <billy1125@gmail.com>
2022-05-16 12:29:34 +0800

    2022.05.16 課程進度

--- MediaPlayer/MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MediaPlayer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MediaPlayer
 {
@@ -28,7 +29,7 @@ namespace MediaPlayer
         private void btnOpenFile_Click(object sender, RoutedEventArgs e)
         {
             // 檔案開啟物件
-            var fd = new Microsoft.Win32.OpenFileDialog();
+            Microsoft.Win32.OpenFileDialog fd = new Microsoft.Win32.OpenFileDialog();
             // 設定檔案過濾，格式：說明文字|*.副檔名
             fd.Filter = "音訊檔案(*.mp3,*.3gp,*.wma)|*.mp3; *.3gp; *.wma|影片檔案(*.mp4, *.avi, *.mpeg, *.wmv)|*.mp4; *.avi; *.mpeg; *.wmv|所有檔案(*.*)|*.*";
             //fd.Filter = "MP3(*.mp3)|*.mp3|MP4(*.mp4)|*.mp4|3GP(*.3gp)|*.3gp|WMA(*.wma)|*.wma|MOV(*.mov)|*.mov|AVI(*.avi)|*.avi|WMV(*.wmv)|*.wmv|MPEG(*.mpeg)|*.mpeg|所有檔案(*.*)|*.*";
@@ -80,6 +81,39 @@ namespace MediaPlayer
         {
             MedShow.Volume = sliVolume.Value;
             //txtFilePath.Text = MedShow.Volume.ToString();
-        }      
+        }
+
+        TimeSpan TimePosition; // 宣告一個時間間格
+        DispatcherTimer timer = null; // 宣告一個「空的」計時器
+
+        private void MedShow_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            // 取得所開啟的影片時間長度
+            TimePosition = MedShow.NaturalDuration.TimeSpan;
+            // 重新設定影片播放滑桿
+            sliProgress.Minimum = 0;
+            sliProgress.Maximum = TimePosition.TotalMilliseconds; //最大值設定為影片的總毫秒數
+
+            // 設定計時器
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // 這個計時器設定每一個刻度為1秒
+            timer.Tick += new EventHandler(timer_tick); //每一個時間刻度設定一個小程序timer_tick
+            timer.Start(); // 啟動這個計時器
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            // 小程序，更新目前影片播放進度
+            sliProgress.Value = MedShow.Position.TotalMilliseconds;
+            txtTime.Text = MedShow.Position.ToString("h'h 'm'm 's's'");
+        }
+
+        private void sliProgress_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int SliderValue = (int)sliProgress.Value; 
+
+            TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue); //將滑桿的數值改變成時間間格的資料形式
+            MedShow.Position = ts; // 調整影片播放進度到新的時間
+        }
     }
 }
